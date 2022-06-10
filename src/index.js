f6c83c0a63e1d09ad246cc608b227a5fd899be47
lucharrier <lucharrier@gmail.com>
2022-06-10 17:17:33 +0200

    Added log

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require( "express" );
 const app = express();
 const port = process.env.PORT; // default port to listen
-
+app.use(express.json())  
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
 
 app.post( "/", ( req, res ) => {
-    console.log('LCS HELLO WORD', req.body)
+    console.log('LCS HELLO WORD', req, req.body)
     res.send( "Hello world!" );
 });
 
