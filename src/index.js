2965f2f592c8825e664805db3c2f39275d922655
lucharrier <lucharrier@gmail.com>
2022-06-10 17:14:24 +0200

    Added post url

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const port = process.env.PORT; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    console.log('LCS HELLO WORD', req.body)
     res.send( "Hello world!" );
 } );
+
+app.post( "/", ( req, res ) => {
+    console.log('LCS HELLO WORD', req.body)
+    res.send( "Hello world!" );
+});
 
 // start the Express server
 app.listen( port, () => {
