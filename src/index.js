54545d987b8fdb804cac3c952dd5a762ec8ee61f
lucharrier <lucharrier@gmail.com>
2022-06-13 10:48:42 +0200

    Added triggers ops

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ app.use(express.json())
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
-app.post( "/", ( req, res ) => {
-    if (!process.env.TOKEN.split(',').includes(req.body.token)) { 
-        return
-    }
-    const triggers = [
+
+const TRIGGERS = {
+    ops: [
+        `nouvelle app sur scalingo`,
+        `nouvelle app Scalingo`,
+        `un nom de domaine en beta.gouv`,
+        `demande d'apps supplémentaire sur scalingo`,
+        `nous aimerions un domaine en .beta.gouv.fr`,
+    ],
+    help: [
         `perdu mes accès à ma boite`,
         `n'arrive plus à accéder à son mail`,
         `n'arrive plus à accéder à son email`,
@@ -35,12 +40,34 @@ app.post( "/", ( req, res ) => {
         `perdu accès à sa boîte`,
         `perdu l'accès à ma boîte`,
         `perdu ses accès`
-    ].map(str => str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
-    const text = req.body.text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-    if (triggers.some(trigger => text.includes(trigger))) {
-        const text = `Hello @${req.body.user_name}, tu sembles avoir un problème fréquent dont la réponse se trouve sans doute dans la doc :
+    ]
+}
+
+const buildText = {
+    help: (params) => {
+        return `Hello @${params.user_name}, tu sembles avoir un problème fréquent dont la réponse se trouve sans doute dans la doc :
 https://doc.incubateur.net/communaute/travailler-a-beta-gouv/jutilise-les-outils-de-la-communaute/problemes-frequents
 N'hésites pas aussi a utiliser la barre de recherche de la doc pour trouver la bonne page.`
+    },
+    ops: (params) => {
+        return `Hello @${params.user_name}, si tu veux faire une demande d'ops (création d'app scalingo/sentry/matomo/domaine) tu peux utiliser le formulaire mentionné dans l'en-tête du canal.`
+    }
+}
+
+app.post( "/", ( req, res ) => {
+    if (!process.env.TOKEN.split(',').includes(req.body.token)) { 
+        return
+    }
+    let type
+    if (req.body.channel_name = 'incubateur-embauche-autre') {
+        type = 'ops'
+    } else {
+        type = 'help'
+    }
+    let triggers = TRIGGERS[type].help.map(str => str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+    const text = req.body.text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    if (triggers.some(trigger => text.includes(trigger))) {
+        const text = buildText(req.body)
         res.json({
             text,
             response_type: 'comment',
