4cd9839377ec57cb0325386223c3d0a8c4c473f2
lucharrier <lucharrier@gmail.com>
2022-06-13 10:00:50 +0200

    Changed text format

--- src/index.js
+++ src/index.js
@@ -38,10 +38,11 @@ app.post( "/", ( req, res ) => {
     ].map(str => str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
     const text = req.body.text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
     if (triggers.some(trigger => text.includes(trigger))) {
+        const text = `Hello @${req.body.user_name}, tu sembles avoir un problème fréquent dont la réponse se trouve sans doute dans la doc : 
+        https://doc.incubateur.net/communaute/travailler-a-beta-gouv/jutilise-les-outils-de-la-communaute/problemes-frequents 
+        N'hésites pas aussi a utiliser la barre de recherche de la doc pour trouver la bonne page.`
         res.json({
-            text: `Hello @${req.body.user_name}, tu sembles avoir un problème fréquent dont la réponse se trouve sans doute dans la doc : 
-            https://doc.incubateur.net/communaute/travailler-a-beta-gouv/jutilise-les-outils-de-la-communaute/problemes-frequents 
-            N'hésites pas aussi a utiliser la barre de recherche de la doc pour trouver la bonne page.`,
+            text,
             response_type: 'comment',
         })
     }
